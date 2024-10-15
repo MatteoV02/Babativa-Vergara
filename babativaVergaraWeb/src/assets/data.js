35b8ddcabec801ff66adea731a86c2e39d676918
TavoTksh1 <tavo.tkshi@gmail.com>
2024-10-15 18:17:37 -0500

    Este Si

---
+++ babativaVergaraWeb/src/assets/data.js
@@ -0,0 +1,94 @@
+import { 
+    faGavel, 
+    faUserTie, 
+    faShieldAlt, 
+    faComments, 
+    faMoneyBillWave, 
+    faBalanceScale, 
+    faWallet,
+    faHandshake
+  } from "@fortawesome/free-solid-svg-icons";
+
+export const data = [
+    {
+        id: 1, title:"Babativa & Vergara", desc:"Abogados por profesion, pero tus amigos, consejeros y aliados por vocacion",imgUrl:"src/assets/img/ImagenMateo.jpg"
+    },
+    {
+        id: 0, title:"Cerca de usted", desc:"Con sede en Bogota D.C, pero con alcance a toda la tierra colombiana", imgUrl:"src/assets/img/ImagenBogota.jpg"
+        
+    },
+    {
+        id: 2, title:"Profesionales", desc:"Contamos con años de trayectoria para brindarte la mejor experiencia y servicio", imgUrl:"src/assets/img/ImagenSeria.jpg"
+    }
+];
+
+export const partners = [
+
+    {
+        id:1, title:"Banco Occidente", logo:"src/assets/img/LogoBancoOccidente2.png", url:"https://www.bancodeoccidente.com.co/wps/portal/banco-de-occidente/bancodeoccidente/"
+    },
+    {
+        id:2, title:"Banco Davivienda", logo:"src/assets/img/LogoDavivienda2.png", url:"https://www.davivienda.com/wps/portal/personas/nuevo"
+    },
+    {
+        id:3, title:"Banco Bancoomeva", logo:"src/assets/img/LogoBancoomeva2.png", url:"https://www.bancoomeva.com.co/"
+    }
+];
+
+export const services1 = [
+    {
+        id:1, icon:faUserTie, title:"Derecho laboral"
+    },
+    {
+        id:2, icon:faMoneyBillWave, title:"Derecho Tributario"
+    },
+    {
+        id:3, icon:faGavel, title:"Derecho administrativo"
+    },
+    {
+        id:4, icon:faBalanceScale , title:"Derecho Civil,\n Familiar y Comercial"
+    }
+]
+
+export const services2 = [
+
+    {
+        id:0, icon:faShieldAlt, title:"Superintendencias"
+    },
+    {
+        id:1, icon:faComments, title:"Asesorías Jurídicas"
+    },
+    {
+        id:2, icon:faWallet, title:"Administración y Cobro de Cartera"
+    },
+    {
+        id:3, icon:faHandshake, title:"Prevención \ny Resolución de Controversias"
+    }
+
+]
+
+export const areas = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+        
+    }
+]
+
+
