844e5cba195480712e9301437656062f0aae8e12
TavoTksh1 <tavo.tkshi@gmail.com>
2024-10-24 18:00:01 -0500

    Style servicios

--- babativaVergaraWeb/src/components/Servicess/components/SrvIntro.jsx
+++ babativaVergaraWeb/src/components/Servicess/components/SrvIntro.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function SrvIntro() {
   return (
-    <>
+    <div className='desc5'>
       <p>En Babativa &amp; Vergara, no solo somos abogados; somos tus aliados. Nos comprometemos a brindarte un servicio excepcional, basado en la confianza y el respeto. Permítenos ser parte de tu camino legal y juntos enfrentaremos cualquier desafío.</p>
-      <p>Descubre cómo nuestro equipo de expertos puede ayudarte a enfrentar tus desafíos con confianza y profesionalismo.</p>
-    </>
+      <p><br />Descubre cómo nuestro equipo de expertos puede ayudarte a enfrentar tus desafíos con confianza y profesionalismo.</p>
+    </div>
   )
 }
 
